559c49b13f2c37c3e6bd76f1ac0b9de4853d4400
Björgvin Hrólfsson <bjorgvin@Bjorgvins-MacBook-Pro.local>
2019-09-17 09:43:18 +0000

    lýsing á algrím + byrjaði á function

---
+++ tile_traveller.py
@@ -0,0 +1,11 @@
+#starting by defining the playing field with xy coordinates
+# A function will be used to move the player on the playing field
+# If the move from the function is valid the move will be performed if invalid an error message will appear
+
+def movement (x,y):
+
+
+
+
+
+    
