c3fb1a4e7c5ae6e96464534bdd67ba71724302da
Björgvin Hrólfsson <bjorgvin@Bjorgvins-MacBook-Pro.local>
2019-09-17 10:16:57 +0000

    bjó til function

--- tile_traveller.py
+++ tile_traveller.py
@@ -2,10 +2,27 @@
 # A function will be used to move the player on the playing field
 # If the move from the function is valid the move will be performed if invalid an error message will appear
 
-def movement (x,y):
+def movement (direction):
+    x_movement = 0
+    y_movement = 0
+    if direction =='N':
+        y_movement = 1
+    elif direction == 'S':
+        y_movement = -1
+    elif direction == 'W':
+        x_movement = -1
+    elif direction == 'E':
+        x_movement = 1
+    return (x_movement,y_movement)
+
+#playing field   
+x_ás= 1
+y_ás = 1
+
+
+direction = input ('You can travel: ')
 
 
 
 
 
-    
